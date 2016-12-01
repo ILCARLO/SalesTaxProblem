37e672fe4847c989faf232dfaa7d48dcc30ef5f1
Carlo Menapace <carlo.menapace@factorymind.com>
2016-12-01 10:19:44 +0100

    Missing license

--- SalesTaxProblem.ConsoleApp/Program.cs
+++ SalesTaxProblem.ConsoleApp/Program.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿// ———————————————————————–
+// 
+//  <copyright file="Program.cs" >
+//      Copyright © 2016 All rights reserved. 
+//      Carlo Menapace
+//      01 / 12 / 2016
+//  </copyright>
+// 
+// Copyright  © 2016 Carlo Menapace
+// 
+//  This file is part of SalesTaxProblem.
+// 
+//     SalesTaxProblem free software: you can redistribute it and/or modify
+//     it under the terms of the Affero GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     SalesTaxProblem distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     Affero GNU General Public License for more details.
+// 
+//     You should have received a copy of the Affero GNU General Public License
+//     along with SalesTaxProblem.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// ———————————————————————–
+using System;
 using System.Collections.Generic;
 using SalesTaxProblem.Domain;
 
